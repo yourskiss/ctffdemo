1af0a6b20eb485705ba382d0a8c570dd5cb559b0
K. Singh <16361374+yourskiss@users.noreply.github.com>
2025-02-05 13:59:57 +0530

    ctff boilerplat

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ function App() {
  
   return (
     <>
-      <h1 className="text-xl text-white bg-black">Vite + React</h1>
-       
+      <h1 className="text-xl text-white bg-black body-default">Vite + React</h1>
+      <p className="accent text-black bg-red">
+        sfsdf dsf sdf dsf dsf sfsdf dsf sdf dsf dsf sfsdf dsf sdf dsf dsf sfsdf dsf sdf dsf dsf sfsdf dsf sdf dsf dsf sfsdf dsf sdf dsf dsf sfsdf dsf sdf dsf dsf sfsdf dsf sdf dsf dsf sfsdf dsf sdf dsf dsf sfsdf dsf sdf dsf dsf sfsdf dsf sdf dsf dsf sfsdf dsf sdf dsf dsf sfsdf dsf sdf dsf dsf sfsdf dsf sdf dsf dsf sfsdf dsf sdf dsf dsf sfsdf dsf sdf dsf dsf sfsdf dsf sdf dsf dsf 
+      </p>
     </>
   )
 }
